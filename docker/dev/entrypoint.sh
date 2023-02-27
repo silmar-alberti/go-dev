31fc6489353f7eb7482e5a9af99db7c15dc914d9
Ricardo Kovalski <ricardokovalskicruz@gmail.com>
2023-02-27 16:55:33 -0300

    Corrigindo o arquivo docker-compose.yml; Removendo espaços em branco em entrypoint.sh;

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -4,8 +4,7 @@ echo "valor debug [${DEBUG}]"
 if [ "${DEBUG}" = "true" ]; then
     echo "dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient debug $@"
     dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient debug $@
-else
-    
+else    
     echo "go run $@"
     go run $@
 fi
